2bafe960bb9fc210dc593a5a10b1d98b50db3061
José Emanuel <emanuelcascone@gmail.com>
2021-12-28 10:43:44 +0000

    Single page applications part 1

--- react hong kong/pratice/react-firts-project/confusion/src/components/MainComponent.js
+++ react hong kong/pratice/react-firts-project/confusion/src/components/MainComponent.js
@@ -3,29 +3,38 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
-import { DISHES } from '../shared/Dishes';
+import ContactUs from './ContactComponent'
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 import { Routes, Route, Navigate } from 'react-router-dom'
+
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
+    
         this.state = {
-            dishes: DISHES,
-            selectedDish: null
+          dishes: DISHES,
+          comments: COMMENTS,
+          promotions: PROMOTIONS,
+          leaders: LEADERS
         };
-    }
+      }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
 
     render() {
         const HomePage = () => {
             return (
                 <>
-                    <Home />
-                </>
+         <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />                </>
             )
         }
         return (
@@ -38,7 +47,7 @@ class Main extends Component {
 
                     </Route>
                     <Route path='home' element={<HomePage />} />
-
+                    <Route path='contactus' element={<ContactUs />} />
                     <Route path='menu' element={<Menu dishes={this.state.dishes} />} />
 
                 </Routes>
