d8dd8b926c13502b8be3623c2fd885889a5dc9e8
Emanuel Cascone <emanuelcascone@gmail.com>
2022-01-02 14:37:16 +0100

    cleaning

--- React Front-End/react hong kong/confusion/README.md
+++
@@ -1,4 +0,0 @@
-docker run -dpt 3000:3000 \
-     -w /app -v "$(pwd):/app" \
-     react \
-     sh -c "npm install && npm run start"
--- React Front-End/react hong kong/confusion/src/functions/withRouter.js
+++
@@ -1,9 +0,0 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import React from 'react';
-export function withRouter(Child) {
-    return (props) => {
-        const location = useLocation();
-        const navigate = useNavigate();
-        return <Child {...props} navigate={navigate} location={location} />;
-    }
-}
